471ed33638c621fdfb8c53119e951818d44de35e
George Sydby <george.sydby@gmail.com>
2021-10-18 12:28:05 +0200

    Adding array of currency values to an array moneyValue[].

---
+++ VendingMachine/Model/Money.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine.Model
+{
+    public class Money
+    {
+        //Fields, declare, initialize
+
+        //1kr, 5kr, 10kr, 20kr, 50kr, 100kr, 500kr and 1000kr.
+        private readonly int[] moneyValue = { 1, 5, 10, 20, 50, 100, 1000 };
+        
+    }
+}

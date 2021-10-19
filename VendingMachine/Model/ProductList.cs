1b9bb1e923144c5588f49b535ded44e836f6c760
George Sydby <george.sydby@gmail.com>
2021-10-19 12:44:06 +0200

    Fattar inte, alla exempel av tutorials förklarar inte det jag behöver veta.

--- VendingMachine/Model/ProductList.cs
+++ VendingMachine/Model/ProductList.cs
@@ -10,15 +10,53 @@ namespace VendingMachine.Model
     {
         //Merge product type lists from ICandyProducts, IToyProducts, ISoftDrinkProducts        
 
+        public string Info()
+        {
+            //return $"{IInformation.INFOTEXT}\n {Name}\n {Phone}\n{ContactAdress}";
+            return $"{ICandyProducts.}\n {Name}\n {Phone}\n{ContactAdress}";
+        }
 
-
-
-        //Misslyckade experment
+        //Misslyckade experiment
         //ICandyProducts candyProducts = new ICandyProducts();
         //string[] productList = base.
         //void ICandyProducts.CandyList() {      }
         //void ISoftDrinkProducts.Softdrinks();
 
+        // Administrator = child, Employee = parent
+        class Administrator : Employee
+        {
+            Employee[] employeesList;
+            public int Bonus { get; set; }
+            public Administrator(string name,
+                                string phone,
+                                string adress,
+                                int salery,
+                                string department,
+                                Employee[] employees,
+                                int bonus)
+                : base(name, phone, adress, salery, department)
+            {
+                this.employeesList = employees;
+                Bonus = bonus;
+            }
 
-    }
+            public new string Info()
+            {
+                return base.Info() + $"\nBonus: {Bonus}";
+            }
+            public override string[] TasksToDo()
+            {
+                string[] tasksToReturn = new string[tasks.Length];
+                tasks.CopyTo(tasksToReturn, 0);
+                Array.Resize(ref tasksToReturn, employeesList.Length + tasksToReturn.Length);
+
+                int index = tasks.Length;
+                foreach (Employee employee in employeesList)
+                {
+                    tasksToReturn[index] = "Employee: " + employee.Name;
+                    index++;
+                }
+                return tasksToReturn;
+            }
+        }
 }

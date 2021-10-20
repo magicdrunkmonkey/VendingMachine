702fefea41a1af334bd038c7d52f5ded9bc3ed24
George Sydby <george.sydby@gmail.com>
2021-10-20 16:33:05 +0200

    Updating

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -11,17 +11,17 @@ namespace VendingMachine
 
             VendingMachine.Model.VendingMachine vendingMachine = new VendingMachine.Model.VendingMachine();
 
-            //List of candy
+            //List of candy name, price, type
             CandyProducts candy0 = new CandyProducts("Snickers", 10, "Candy");
             CandyProducts candy1 = new CandyProducts("Cloetta", 12, "Candy");
             CandyProducts candy2 = new CandyProducts("Bounty", 9, "Candy");
 
-            //List of softdrinks
+            //List of softdrinks name, price, type
             SoftDrinkProducts softdrink0 = new SoftDrinkProducts("Pepsi", 8, "Softdrink");
             SoftDrinkProducts softdrink1 = new SoftDrinkProducts("Fanta", 11,"Softdrink");
             SoftDrinkProducts softdrink2 = new SoftDrinkProducts("7up", 13, "Softdrink");
 
-            //List of toys
+            //List of toys name, price, type
             ToyProducts toy0 = new ToyProducts("Pepsi", 8, "Toy");
             ToyProducts toy1 = new ToyProducts("Fanta", 11, "Toy");
             ToyProducts toy2 = new ToyProducts("7up", 13, "Toy");
